3b09831c3484b05a3cf02ee55c493a1a0cd34f18
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2023-12-01 19:53:31 -0600

    Add alerts if invalid data entered in share movie form

--- src/AddMovieFormModal.jsx
+++ src/AddMovieFormModal.jsx
@@ -1,24 +1,79 @@
-import { Modal, Button, Form, Container } from 'react-bootstrap';
+import { Modal, Button, Form, Container, Alert } from 'react-bootstrap';
+import { useState } from 'react';
 import styles from './HomePage.module.css';
 
 
 function AddMovieFormModal({ showModal, handleCloseModal, postMovie }) {
+
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertContent, setAlertContent] = useState('');
+
+  // Check if movie trailer is a valid YouTube link
+  const isValidYouTubeURL = (url) => {
+
+    // Regular expression to match the YouTube URL pattern
+    const youtubeURLRegex = /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.be)\/.+$/;
+    // Test the URL against the regex pattern
+    return youtubeURLRegex.test(url);
+
+  };
+
+  // Convert url into embeddable format by extracting videoID from Youtube URL
+  const convertToEmbedURL = (youtubeURL) => {
+
+    // search for characters after 'v=' and ignore once reaches '&'
+    let regex = /[?&]v=([^&]+)/;
+    let match = youtubeURL.match(regex);
+
+    if (match) {
+      return `https://www.youtube.com/embed/${match[1]}`;
+    } else {
+      // Handle invalid or unsupported URLs
+      console.error('Invalid YouTube URL:', youtubeURL);
+      return null;
+    }
+  };
 
   // Handle share book button
   const submitHandler = (e) => {
     e.preventDefault();
     const form = e.target;
     
+    // Extract form data
     const newMovie = {
       movieName: form.movieName.value,
       userComment: form.userComment.value,
       videoLink: form.videoLink.value,
       genre: form.genre.value,
+    };
+
+    // Validation checks
+    if (!newMovie.movieName || !newMovie.userComment || !newMovie.videoLink || !newMovie.genre) {
+      setAlertContent('Please fill in all fields.');
+      setShowAlert(true);
+      return;
     }
 
+    if (!isValidYouTubeURL(newMovie.videoLink)) {
+      setAlertContent('Please enter a valid YouTube link.');
+      setShowAlert(true);
+      return;
+    }
+
+    const embedURL = convertToEmbedURL(newMovie.videoLink);
+    if (!embedURL) {
+      setAlertContent('Failed to convert YouTube URL to an embeddable format.');
+      setShowAlert(true);
+      return;
+    }
+
+    newMovie.videoLink = embedURL;
+    
+    // Post movie
     console.log(newMovie);
     postMovie(newMovie);
     handleCloseModal(); 
+    setShowAlert(false);
   }
 
 
@@ -31,6 +86,11 @@ function AddMovieFormModal({ showModal, handleCloseModal, postMovie }) {
 
         <Modal.Body>
           <Container>
+            {showAlert && (
+              <Alert variant="danger" onClose={() => setShowAlert(false)} dismissible>
+                {alertContent}
+              </Alert>
+            )}
             <Form onSubmit={submitHandler}>
               <Form.Group controlId="movieName">
                 <Form.Label></Form.Label>
